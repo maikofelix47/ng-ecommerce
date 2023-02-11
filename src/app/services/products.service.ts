eb444e469e7d9cb31e3a1d0815c03a1781209744
maikofelix47 <maikofelix89@gmail.com>
2023-02-11 14:55:58 +0300

    Modified product service and product details service to use nest backend

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -6,22 +6,20 @@ import { map } from 'rxjs/operators';
 
 import { Product } from '../models/product';
 
-import { firebaseDbUrl } from 'environment';
+import { nestBaseUrl } from 'environment';
 
 @Injectable()
 export class ProductsService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  getProductByCategoryId(categoryId: number): Observable<Product[]> {
+    const url = nestBaseUrl + `/product/category/${categoryId}`;
+    return this.http.get(url).pipe(
+      map((results: any) => {
+        const products: Product[] = Object.values(results);
 
-  getProductByCategoryId(categoryId: number): Observable<Product[]>{
-      const url = firebaseDbUrl + '/products.json';
-      const params = new HttpParams().set('orderBy', '"categoryId"').set('equalTo', categoryId);
-      return this.http.get(url, {
-        params: params
-      }).pipe(map ((results: any)=> {
-          const products: Product[] = Object.values(results);
-          
-          return products;
-      }));
+        return products;
+      })
+    );
   }
 }
